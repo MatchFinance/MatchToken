e8b1d7959bf770d58b1522a7d4cc35d5af480070
liyang <ylikp@connect.ust.hk>
2023-12-04 14:30:49 +0800

    feat: update match token roles

--- contracts/MatchToken.sol
+++ contracts/MatchToken.sol
@@ -2,48 +2,47 @@
 
 pragma solidity =0.8.21;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MatchToken is ERC20, Ownable {
-    mapping(address user => uint256 role) public userRoles;
+    mapping(address => bool) public isMinter;
+    mapping(address => bool) public isBurner;
 
     event Mint(address indexed to, uint256 amount);
     event Burn(address indexed from, uint256 amount);
 
-    constructor() ERC20("Match Token", "MATCH") Ownable(msg.sender) {
-        _mint(msg.sender, 100 ether);
-    }
+    constructor() ERC20("Match Token", "MATCH") Ownable(msg.sender) {}
 
     function decimals() public pure override returns (uint8) {
         return 18;
     }
 
     function addMinter(address _newMinter) external onlyOwner {
-        userRoles[_newMinter] = 1;
+        isMinter[_newMinter] = true;
     }
 
     function removeMinter(address _minter) external onlyOwner {
-        userRoles[_minter] = 0;
+        isMinter[_minter] = false;
     }
 
     function addBurner(address _newBurner) external onlyOwner {
-        userRoles[_newBurner] = 2;
+        isBurner[_newBurner] = true;
     }
 
     function removeBurner(address _burner) external onlyOwner {
-        userRoles[_burner] = 0;
+        isBurner[_burner] = false;
     }
 
     function mint(address _to, uint256 _amount) external {
-        require(userRoles[msg.sender] == 1, "Not a minter");
+        require(isMinter[msg.sender], "Not a minter");
         _mint(_to, _amount);
 
         emit Mint(_to, _amount);
     }
 
     function burn(address _from, uint256 _amount) external {
-        require(userRoles[msg.sender] == 2, "Not a burner");
+        require(isBurner[msg.sender], "Not a burner");
         _burn(_from, _amount);
 
         emit Burn(_from, _amount);
