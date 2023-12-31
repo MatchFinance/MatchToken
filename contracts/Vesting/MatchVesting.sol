72e802abb1fc2da680a49eb4370b12fc8c7eacfa
liyang <ylikp@connect.ust.hk>
2023-12-31 15:16:43 +0800

    feat: update match vesting

--- contracts/Vesting/MatchVesting.sol
+++ contracts/Vesting/MatchVesting.sol
@@ -7,7 +7,7 @@ import { ReentrancyGuardUpgradeable } from "@openzeppelin/contracts-upgradeable/
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-contract MatchKOLVesting is OwnableUpgradeable, ReentrancyGuardUpgradeable {
+contract MatchVesting is OwnableUpgradeable, ReentrancyGuardUpgradeable {
     using SafeERC20 for IERC20;
 
     // ---------------------------------------------------------------------------------------- //
@@ -24,30 +24,30 @@ contract MatchKOLVesting is OwnableUpgradeable, ReentrancyGuardUpgradeable {
 
     struct VestingInfo {
         uint256 start;
-        uint256 cliff;
-        uint256 duration;
+        uint256 cliff; // If have lock period, cliff = start + lock period
+        uint256 duration; // Duration starts from "start time" (not "cliff time")
         uint256 interval;
-        uint256 amount;
-        uint256 tgeAmount;
-        uint256 vestedAmount;
-        uint256 vestedPerTime;
+        uint256 amount; // Total amount
+        uint256 tgeAmount; // Amount released at TGE
+        uint256 vestedAmount; // Already vested
+        uint256 vestedPerTime; // Vested amount each time
     }
-    mapping(address kol => VestingInfo vestingInfo) public vestings;
+    mapping(address user => VestingInfo vestingInfo) public vestings;
 
     // ---------------------------------------------------------------------------------------- //
     // *************************************** Events ***************************************** //
     // ---------------------------------------------------------------------------------------- //
 
     event NewVestingSet(
-        address investor,
+        address receiver,
         uint256 start,
         uint256 cliff,
         uint256 duration,
         uint256 interval,
-        uint256 totalAmount,
-        uint256 TGERelease
+        uint256 amount,
+        uint256 tgeAmount
     );
-    event StopVesting(address investor);
+    event VestingStopped(address investor);
     event Withdraw(address investor, uint256 amount);
 
     // ---------------------------------------------------------------------------------------- //
@@ -65,40 +65,48 @@ contract MatchKOLVesting is OwnableUpgradeable, ReentrancyGuardUpgradeable {
     // ************************************ Main Functions ************************************ //
     // ---------------------------------------------------------------------------------------- //
 
+    // ! If a receiver's vesting is "all released at tge"
+    // ! Should set it with parameters:
+    // !  -> start = cliff 
+    // !  -> duration = 0 & inteval = 0
+    // !  -> amount = tgeAmount
     function setNewVesting(
-        address _kol,
+        address _receiver,
         uint256 _start,
         uint256 _cliff,
         uint256 _duration,
         uint256 _interval,
-        uint256 _totalAmount,
+        uint256 _amount,
         uint256 _tgeAmount
     ) public onlyOwner {
-        require(_totalAmount > 0, "Amount must be greater than zero");
+        require(_amount > 0, "Amount must be greater than zero");
+        require(_amount >= _tgeAmount, "Amount must be greater than tge");
         require(_start >= TGE, "Should start at or after TGE");
         require(_start <= _cliff, "Cliff must be after start");
         require(_start + _duration >= block.timestamp, "Vesting must end in the future");
 
-        VestingInfo storage newVesting = vestings[_kol];
-
+        // Store the new vesting info
+        VestingInfo storage newVesting = vestings[_receiver];
         newVesting.start = _start;
         newVesting.cliff = _cliff;
         newVesting.duration = _duration;
         newVesting.interval = _interval;
-        newVesting.amount = _totalAmount;
+        newVesting.amount = _amount;
         newVesting.tgeAmount = _tgeAmount;
 
         // Total times of distribution
-        uint256 totalTimes = ((_duration - (_cliff - _start)) / _interval) + 1;
-        newVesting.vestedPerTime = (_totalAmount - _tgeAmount) / totalTimes;
+        if (_interval > 0) {
+            uint256 totalTimes = ((_duration - (_cliff - _start)) / _interval) + 1;
+            newVesting.vestedPerTime = (_amount - _tgeAmount) / totalTimes;
+        }
 
-        emit NewVestingSet(_kol, _start, _cliff, _duration, _interval, _totalAmount, _tgeAmount);
+        emit NewVestingSet(_receiver, _start, _cliff, _duration, _interval, _amount, _tgeAmount);
     }
 
-    function setNewVestings(VestingInfo[] memory _vestings, address[] memory _kols) external onlyOwner {
+    function setNewVestings(VestingInfo[] memory _vestings, address[] memory _receivers) external onlyOwner {
         for (uint256 i; i < _vestings.length; ) {
             setNewVesting(
-                _kols[i],
+                _receivers[i],
                 _vestings[i].start,
                 _vestings[i].cliff,
                 _vestings[i].duration,
@@ -112,11 +120,13 @@ contract MatchKOLVesting is OwnableUpgradeable, ReentrancyGuardUpgradeable {
         }
     }
 
-    function stopVesting(address _kol) external onlyOwner {
-        require(vestings[_kol].amount > 0, "Not an active investor");
+    // Stop a user's vesting
+    // When updating vesting info, stop it and set a new one
+    function stopVesting(address _receiver) external onlyOwner {
+        require(vestings[_receiver].amount > 0, "Not an active investor");
 
-        delete vestings[_kol];
-        emit StopVesting(_kol);
+        delete vestings[_receiver];
+        emit VestingStopped(_receiver);
     }
 
     function claim() external nonReentrant {
