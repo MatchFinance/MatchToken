f6cb88a60d291c3790bd6f9bfbf3caabf72cb88a
liyang <ylikp@connect.ust.hk>
2024-01-02 16:01:37 +0800

    feat: Match token ready for mainnet

--- contracts/MatchToken.sol
+++ contracts/MatchToken.sol
@@ -1,3 +1,12 @@
+/*
+ *  ███╗   ███╗ █████╗ ████████╗ ██████╗██╗  ██╗    ███████╗██╗███╗   ██╗ █████╗ ███╗   ██╗ ██████╗███████╗  *
+ *  ████╗ ████║██╔══██╗╚══██╔══╝██╔════╝██║  ██║    ██╔════╝██║████╗  ██║██╔══██╗████╗  ██║██╔════╝██╔════╝  *
+ *  ██╔████╔██║███████║   ██║   ██║     ███████║    █████╗  ██║██╔██╗ ██║███████║██╔██╗ ██║██║     █████╗    *
+ *  ██║╚██╔╝██║██╔══██║   ██║   ██║     ██╔══██║    ██╔══╝  ██║██║╚██╗██║██╔══██║██║╚██╗██║██║     ██╔══╝    *
+ *  ██║ ╚═╝ ██║██║  ██║   ██║   ╚██████╗██║  ██║    ██║     ██║██║ ╚████║██║  ██║██║ ╚████║╚██████╗███████╗  *
+ *  ╚═╝     ╚═╝╚═╝  ╚═╝   ╚═╝    ╚═════╝╚═╝  ╚═╝    ╚═╝     ╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝╚═╝  ╚═══╝ ╚═════╝╚══════╝  *
+ */
+
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 pragma solidity =0.8.21;
@@ -5,27 +14,69 @@ pragma solidity =0.8.21;
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
+/**
+ * @title MatchToken
+ *        (Not Upgradeable)
+ *
+ * @dev   Match token is the governance token of Match Finance.
+ *        It can have an owner and some minters.
+ *
+ *        Owner can: add or remove minters
+ *        Minters can: mint tokens
+ *        (Current minters: Owner)
+ */
+
 contract MatchToken is ERC20, Ownable {
+    // ---------------------------------------------------------------------------------------- //
+    // ************************************* Constants **************************************** //
+    // ---------------------------------------------------------------------------------------- //
+
     // Total supply cap is 10 million
+    // Can not mint more tokens when reaching this cap
     uint256 public constant TOTAL_SUPPLY = 10_000_000 ether;
 
+    // ---------------------------------------------------------------------------------------- //
+    // ************************************* Variables **************************************** //
+    // ---------------------------------------------------------------------------------------- //
+
     mapping(address user => bool isMinter) public isMinter;
-    mapping(address user => bool isBurner) public isBurner;
+
+    // ---------------------------------------------------------------------------------------- //
+    // *************************************** Events ***************************************** //
+    // ---------------------------------------------------------------------------------------- //
 
     event MinterAdded(address indexed newMinter);
     event MinterRemoved(address indexed removedMinter);
-
-    event BurnerAdded(address indexed newBurner);
-    event BurnerRemoved(address indexed removedBurner);
-
     event Mint(address indexed to, uint256 amount);
-    event Burn(address indexed from, uint256 amount);
 
-    constructor() ERC20("Match Token", "MATCH") Ownable(msg.sender) {}
+    // ---------------------------------------------------------------------------------------- //
+    // *************************************** Errors ***************************************** //
+    // ---------------------------------------------------------------------------------------- //
+
+    error NotMinter();
+    error OverCap();
+
+    // ---------------------------------------------------------------------------------------- //
+    // ************************************* Constructor ************************************** //
+    // ---------------------------------------------------------------------------------------- //
+
+    constructor() ERC20("Match Token", "MATCH") Ownable(msg.sender) {
+        // ! The owner is set as an initial minter to distribute tokens to IDO contracts
+        // ! Should remove this power after these operations
+        isMinter[msg.sender] = true;
+    }
+
+    // ---------------------------------------------------------------------------------------- //
+    // *********************************** View Functions ************************************* //
+    // ---------------------------------------------------------------------------------------- //
 
     function decimals() public pure override returns (uint8) {
         return 18;
     }
+
+    // ---------------------------------------------------------------------------------------- //
+    // *********************************** Set Functions ************************************** //
+    // ---------------------------------------------------------------------------------------- //
 
     function addMinter(address _newMinter) external onlyOwner {
         isMinter[_newMinter] = true;
@@ -39,32 +90,16 @@ contract MatchToken is ERC20, Ownable {
         emit MinterRemoved(_minter);
     }
 
-    function addBurner(address _newBurner) external onlyOwner {
-        isBurner[_newBurner] = true;
-
-        emit BurnerAdded(_newBurner);
-    }
-
-    function removeBurner(address _burner) external onlyOwner {
-        isBurner[_burner] = false;
-
-        emit BurnerRemoved(_burner);
-    }
+    // ---------------------------------------------------------------------------------------- //
+    // *********************************** Main Functions ************************************* //
+    // ---------------------------------------------------------------------------------------- //
 
     function mint(address _to, uint256 _amount) external {
-        require(isMinter[msg.sender], "Not a minter");
-        require(totalSupply() + _amount <= TOTAL_SUPPLY, "Total supply cap exceeded");
+        if (!isMinter[msg.sender]) revert NotMinter();
+        if (totalSupply() + _amount > TOTAL_SUPPLY) revert OverCap();
 
         _mint(_to, _amount);
 
         emit Mint(_to, _amount);
-    }
-
-    function burn(address _from, uint256 _amount) external {
-        require(isBurner[msg.sender], "Not a burner");
-        
-        _burn(_from, _amount);
-
-        emit Burn(_from, _amount);
     }
 }
